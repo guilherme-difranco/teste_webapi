4da64e4a664efd73ce49a03750989cef764beb91
guilherme-difranco <57638011+guilherme-difranco@users.noreply.github.com>
2019-11-11 16:04:01 -0300

    1

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -12,17 +12,51 @@ namespace UnitTest
         [TestMethod]
         public void TestaMetodolcularJurosValido()
         {
-
+            List<ResponseModel> atual = new List<ResponseModel>();
+            List<ResponseModel> esperada = new List<ResponseModel>();
 
             var servico = new JurosService();
             var resultado = servico.CalcularJuros(1, 12);
             atual.Add(resultado);
 
-            ResponseModel saida =  ResponseModel.Factory.Create(true, "1,12", "");
+            ResponseModel saida = ResponseModel.Factory.Create(true, "1,12", "");
             esperada.Add(saida);
-            
+
             esperada.Should().BeEquivalentTo(atual);
         }
+        [TestMethod]
+        public void TestaMetodolcularValorInalido()
+        {
+            List<ResponseModel> atual = new List<ResponseModel>();
+            List<ResponseModel> esperada = new List<ResponseModel>();
+
+            var servico = new JurosService();
+            var resultado = servico.CalcularJuros(0, 12);
+            atual.Add(resultado);
+
+            ResponseModel saida = ResponseModel.Factory.Create(false, "", "ValorInicial deve ser maior que zero.");
+            esperada.Add(saida);
+
+            esperada.Should().BeEquivalentTo(atual);
+        }
+
+        [TestMethod]
+        public void TestaMetodolcularMesesInalido()
+        {
+            List<ResponseModel> atual = new List<ResponseModel>();
+            List<ResponseModel> esperada = new List<ResponseModel>();
+
+            var servico = new JurosService();
+            var resultado = servico.CalcularJuros(1, 0);
+            atual.Add(resultado);
+
+            ResponseModel saida = ResponseModel.Factory.Create(false, "", "Meses deve ser maior que zero.");
+            esperada.Add(saida);
+
+            esperada.Should().BeEquivalentTo(atual);
+        }
+
+
 
 
     }
